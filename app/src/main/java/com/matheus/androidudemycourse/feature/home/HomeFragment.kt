2c3f7c3a02eb68ca2e4857a28299107bd889b1d8
Matheus <contact.matheus.mendes@gmail.com.com>
2021-03-08 22:30:16 -0300

    HomeFragment refactored

--- app/src/main/java/com/matheus/androidudemycourse/feature/home/HomeFragment.kt
+++ app/src/main/java/com/matheus/androidudemycourse/feature/home/HomeFragment.kt
@@ -9,23 +9,23 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.matheus.androidudemycourse.R
+import com.matheus.androidudemycourse.databinding.ActivityMainBinding
+import com.matheus.androidudemycourse.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
