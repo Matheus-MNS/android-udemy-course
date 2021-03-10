ab9c1d7c59c1a23575ea689a00a8f2a29297cb24
Matheus <contact.matheus.mendes@gmail.com.com>
2021-03-09 23:00:35 -0300

    Daily quotes implemented

--- app/src/main/java/com/matheus/androidudemycourse/MainActivity.kt
+++ app/src/main/java/com/matheus/androidudemycourse/MainActivity.kt
@@ -35,7 +35,8 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home,
-                R.id.nav_sort_number
+                R.id.nav_sort_number,
+                R.id.nav_daily_quotes
             ), binding.drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/matheus/androidudemycourse/feature/daily_quotes/DailyQuotesFragment.kt
+++ app/src/main/java/com/matheus/androidudemycourse/feature/daily_quotes/DailyQuotesFragment.kt
@@ -7,6 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.matheus.androidudemycourse.R
 import com.matheus.androidudemycourse.databinding.FragmentDailyQuotesBinding
+import java.util.*
+import kotlin.random.Random
+import kotlin.random.Random.Default.nextInt
 
 
 class DailyQuotesFragment : Fragment() {
@@ -18,17 +21,28 @@ class DailyQuotesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentDailyQuotesBinding.inflate(inflater,container,false)
+        binding = FragmentDailyQuotesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
-
+        binding.newQuotesButton.setOnClickListener {
+            sortQuotes(quotes)
+        }
     }
 
+    val quotes: Array<String> = arrayOf(
+        "Você é o único motivo do seu fracasso.",
+        "Desista, ainda da tempo de não passar vergonha!!",
+        "Desista no primeiro obstáculo!",
+        "Hoje foi ruim, amanhã será péssimo!!",
+        "Agora parece que piorou"
+    )
 
+    fun sortQuotes(agrs: Array<String>) {
+        val i = Random.nextInt(0, 4)
+        binding.quotesTextView.text = quotes[i]
+    }
 }
