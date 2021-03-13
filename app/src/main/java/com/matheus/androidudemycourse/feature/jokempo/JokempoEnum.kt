9eb22ecd27d30d32e0c918aac7895b0c3440f651
Matheus <contact.matheus.mendes@gmail.com.com>
2021-03-13 16:12:55 -0300

    Jokempo implemented

---
+++ app/src/main/java/com/matheus/androidudemycourse/feature/jokempo/JokempoEnum.kt
@@ -0,0 +1,8 @@
+package com.matheus.androidudemycourse.feature.jokempo
+
+enum class JokempoEnum(val label: String) {
+
+    ROCK("Pedra"),
+    PAPER("Papel"),
+    SCISSOR("Tesoura")
+}
--- app/src/main/java/com/matheus/androidudemycourse/feature/jokempo/JokempoFragment.kt
+++ app/src/main/java/com/matheus/androidudemycourse/feature/jokempo/JokempoFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.matheus.androidudemycourse.R
 import com.matheus.androidudemycourse.databinding.FragmentJokempoBinding
+import kotlin.random.Random
 
 class JokempoFragment : Fragment() {
 
@@ -17,12 +18,62 @@ class JokempoFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentJokempoBinding.inflate(inflater,container,false)
+        binding = FragmentJokempoBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupClick()
     }
-  }
+
+    private fun setupClick() {
+
+        binding.rockImageView.setOnClickListener {
+            optionSelected(JokempoEnum.ROCK)
+        }
+
+        binding.paperImageView.setOnClickListener {
+            optionSelected(JokempoEnum.PAPER)
+        }
+
+        binding.scissorsImageView.setOnClickListener {
+            optionSelected(JokempoEnum.SCISSOR)
+        }
+    }
+
+    private fun optionSelected(playerChoice: JokempoEnum) {
+
+        val r = Random.nextInt(0, 3)
+        val optionList = listOf(JokempoEnum.ROCK, JokempoEnum.PAPER, JokempoEnum.SCISSOR)
+        val appChoice = optionList[r]
+
+        when (appChoice) {
+
+            JokempoEnum.ROCK -> binding.placeHolderOpponentImageView.setImageResource(R.drawable.rock)
+            JokempoEnum.PAPER -> binding.placeHolderOpponentImageView.setImageResource(R.drawable.paper)
+            JokempoEnum.SCISSOR -> binding.placeHolderOpponentImageView.setImageResource(R.drawable.scissors)
+
+        }
+        if ( //App Wins
+            (appChoice == JokempoEnum.SCISSOR && playerChoice == JokempoEnum.PAPER) ||
+            (appChoice == JokempoEnum.PAPER && playerChoice == JokempoEnum.ROCK) ||
+            (appChoice == JokempoEnum.ROCK && playerChoice == JokempoEnum.SCISSOR)
+        ) {
+            binding.instructionTextView.text = getString(R.string.user_lose_text)
+
+        } else if ( //User Wins
+            (playerChoice == JokempoEnum.SCISSOR && appChoice == JokempoEnum.PAPER) ||
+            (playerChoice == JokempoEnum.PAPER && appChoice == JokempoEnum.ROCK) ||
+            (playerChoice == JokempoEnum.ROCK && appChoice == JokempoEnum.SCISSOR)
+        ) {
+
+            binding.instructionTextView.text = getString(R.string.user_wins_text)
+
+        } else { //Draw
+            binding.instructionTextView.text = getString(R.string.draw_text)
+        }
+    }
+}
 
