8b312879b095fbcfe73149213aceca50d5b02c9a
Matheus <contact.matheus.mendes@gmail.com.com>
2021-03-27 16:55:07 -0300

    Heads or tails implemented

--- app/src/main/java/com/matheus/androidudemycourse/feature/heads_or_tails/HeadsOrTailsFragment.kt
+++ app/src/main/java/com/matheus/androidudemycourse/feature/heads_or_tails/HeadsOrTailsFragment.kt
@@ -1,23 +1,37 @@
 package com.matheus.androidudemycourse.feature.heads_or_tails
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
 import com.matheus.androidudemycourse.R
-import com.matheus.androidudemycourse.databinding.FragmentHeadsorTailsBinding
+import com.matheus.androidudemycourse.databinding.FragmentHeadsOrTailsBinding
+import kotlin.random.Random
+
 
 class HeadsOrTailsFragment : Fragment() {
 
-    private lateinit var binding: FragmentHeadsorTailsBinding
+    private lateinit var binding: FragmentHeadsOrTailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View?{
-        binding = FragmentHeadsorTailsBinding.inflate(inflater,container,false)
+    ): View {
+        binding = FragmentHeadsOrTailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.playButtom.setOnClickListener {
+
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.nav_host_fragment, HeadsOrTailsResultFragment())
+                ?.commit()
+        }
     }
 }
--- app/src/main/java/com/matheus/androidudemycourse/feature/heads_or_tails/HeadsOrTailsResultFragment.kt
+++ app/src/main/java/com/matheus/androidudemycourse/feature/heads_or_tails/HeadsOrTailsResultFragment.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.matheus.androidudemycourse.R
 import com.matheus.androidudemycourse.databinding.FragmentHeadsOrTailsResultBinding
+import kotlin.random.Random
 
 class HeadsOrTailsResultFragment : Fragment() {
 
     private lateinit var binding: FragmentHeadsOrTailsResultBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,4 +22,25 @@ class HeadsOrTailsResultFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        flipCoin()
+
+//        binding.returnButon.setOnClickListener {
+//
+//            activity?.supportFragmentManager?.beginTransaction()
+//                ?.replace(R.id.nav_host_fragment, HeadsOrTailsResultFragment())
+//                ?.commit()
+//        }
+    }
+
+    private fun flipCoin(){
+        val r =  Random.nextInt(0,1)
+
+        return if (r == 0)
+            binding.resultCoinImageView.setImageResource(R.drawable.heads_coin)
+        else
+            binding.resultCoinImageView.setImageResource(R.drawable.tails_coin)
+    }
 }
