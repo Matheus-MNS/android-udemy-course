b1827d7f531ab88bb25400f39324551d594b5b0d
Matheus <contact.matheus.mendes@gmail.com.com>
2021-03-27 00:22:49 -0300

    Heads or tails interface created

--- app/src/main/java/com/matheus/androidudemycourse/feature/heads_or_tails/HeadsOrTailsResultFragment.kt
+++ app/src/main/java/com/matheus/androidudemycourse/feature/heads_or_tails/HeadsOrTailsResultFragment.kt
@@ -6,18 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.matheus.androidudemycourse.R
-import com.matheus.androidudemycourse.databinding.FragmentHeadsorTailsBinding
+import com.matheus.androidudemycourse.databinding.FragmentHeadsOrTailsResultBinding
 
-class HeadsOrTailsFragment : Fragment() {
+class HeadsOrTailsResultFragment : Fragment() {
 
-    private lateinit var binding: FragmentHeadsorTailsBinding
-
+    private lateinit var binding: FragmentHeadsOrTailsResultBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View?{
-        binding = FragmentHeadsorTailsBinding.inflate(inflater,container,false)
+    ): View {
+        binding = FragmentHeadsOrTailsResultBinding.inflate(inflater,container,false)
         return binding.root
     }
+
 }
