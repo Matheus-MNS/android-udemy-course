9f2d9ea9cf39d835aa3413044bc1aaba9cc91fb3
Matheus <contact.matheus.mendes@gmail.com.com>
2021-03-31 14:03:12 -0300

    ATM consultancy created

---
+++ app/src/main/java/com/matheus/androidudemycourse/feature/atm_consultancy/AtmConsultancy.kt
@@ -0,0 +1,57 @@
+package com.matheus.androidudemycourse.feature.atm_consultancy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.snackbar.Snackbar
+import com.matheus.androidudemycourse.R
+import com.matheus.androidudemycourse.databinding.ActivityAtmConsultancyBinding
+
+
+class AtmConsultancy : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityAtmConsultancyBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAtmConsultancyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMainAtm.toolbarAtm)
+
+//        binding.appBarMainAtm.fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.nav_alcohol_or_gasoline,
+                R.id.nav_jokempo,
+                R.id.nav_daily_quotes,
+                R.id.nav_sort_number
+            ), binding.drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
