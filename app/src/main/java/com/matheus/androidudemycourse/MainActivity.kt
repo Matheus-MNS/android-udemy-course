232098fd0469fc274f3fd594e20aaa06512b8f69
Matheus <contact.matheus.mendes@gmail.com.com>
2021-05-18 20:11:35 -0300

    PR resolved

--- app/src/main/java/com/matheus/androidudemycourse/MainActivity.kt
+++ app/src/main/java/com/matheus/androidudemycourse/MainActivity.kt
@@ -60,6 +60,4 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
-
 }
